f00906240b05fd18ad249f0891e62b0dba81103b
Andre Ramoni <nospam@Ramoni.local>
2019-01-22 20:20:37 -0200

    .

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "KIAIXCHOV7MLFT7KZOQ"
-  secret_key = "a04M7WXNr3FPb2QiY/L82TNPC/ydV7I8H/oNtWF"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
 
