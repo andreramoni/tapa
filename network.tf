4be061a2f57215412e37ee4523e545272c9b32f7
Andre Ramoni <nospam@Ramoni.local>
2019-01-22 23:24:58 -0200

    .

--- network.tf
+++ network.tf
@@ -1,10 +1,46 @@
 #### VPCs
-resource "aws_vpc" "app" {
+resource "aws_vpc" "app01" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags {
-    Name = "${var.app}-${var.env}"
+    Name = "app01"
   }
 }
 
+#### Subnets
+resource "aws_subnet" "app01" {
+  vpc_id                  = "${aws_vpc.app01.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags {
+    Name = "app01-az1"
+  }
+}
+
+#### Internet gateways
+## IG prod
+resource "aws_internet_gateway" "app01" {
+  vpc_id = "${aws_vpc.app01.id}"
+  tags {
+    Name = "app01"
+  }
+}
+
+#### Route tables
+resource "aws_route_table" "app01" {
+  vpc_id = "${aws_vpc.app01.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.app01.id}"
+  }
+  tags {
+    Name = "app01"
+  }
+}
+
+resource "aws_route_table_association" "app01" {
+  subnet_id      = "${aws_subnet.app01.id}"
+  route_table_id = "${aws_route_table.app01.id}"
+}
 
--- variables.tf
+++ variables.tf
@@ -8,12 +8,24 @@ variable "env" {
   default = "stg"
 }
 
-
 variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+
+
+variable "zone" {
   type = "map"
   default = {
-    stg = "10.0.0.0/24"
-    prd = "10.0.0.0/16"
+    stg = "us-east-1"
+  }
+}
+
+variable "subnet_cidr" {
+  type = "map"
+  default = {
+    az1 = "10.0.1.0/24"
+    az2 = "10.0.2.0/24"
   }
 }
 
