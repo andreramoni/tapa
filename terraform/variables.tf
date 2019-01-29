cc1982e84a0cfcbd077192ba738235db4a2a02b4
Andre Guimaraes <andre.ramoni@gmail.com>
2019-01-29 17:34:33 -0200

    .

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,12 @@
 variable "ami_id" {}
-variable "app" {
-  type = "string"
-  default = "app01"
-}
-
-variable "env" {
-  type = "string"
-  default = "stg"
-}
 
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-
-
-variable "zone" {
-  type = "map"
-  default = {
-    stg = "us-east-1"
-  }
-}
+variable "region" {}
 
 variable "subnet_cidr" {
-  type = "map"
-  default = {
-    az1 = "10.0.1.0/24"
-    az2 = "10.0.2.0/24"
-  }
+  default = "10.0.0.0/24"
 }
 
-#variable "zones" {
-#  default = ["us-east-1a", "us-east-1b"]
-#}
