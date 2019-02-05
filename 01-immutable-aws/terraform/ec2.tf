0fd025d7dec419fd531fdc9acc6632c0fead66a5
Andre Ramoni <nospam@Ramoni.local>
2019-02-05 20:47:19 -0200

    .

--- 01-immutable-aws/terraform/ec2.tf
+++ 01-immutable-aws/terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "app01" {
-  ami                    = "${var.ami_id}"
+  ami                    = "ami-054e4601796d80311"
   count                  = "${var.instance_count_desired}"  
   instance_type          = "${var.instance_type}"
   vpc_security_group_ids = [ "${aws_security_group.app01.id}" ]
--- 01-immutable-aws/terraform/securitygroups.tf
+++ 01-immutable-aws/terraform/securitygroups.tf
@@ -15,3 +15,20 @@ resource "aws_security_group" "app01" {
  }
 }
 
+resource "aws_security_group" "app01_elb" {
+  name = "app01_elb"
+  vpc_id = "${aws_vpc.app01.id}"
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
