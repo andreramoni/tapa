088ab2b1ddd194473d657e498aa62926812e1982
Andre Ramoni <nospam@Ramoni.local>
2019-03-01 15:34:16 -0300

    .

--- 301-terraform+aws+packer+ansible/terraform/network.tf
+++ 301-terraform+aws+packer+ansible/terraform/network.tf
@@ -11,13 +11,25 @@ output "vpc_id" { value = "${aws_vpc.vpc01.id}" }
 #### Subnets
 resource "aws_subnet" "subnet01" {
   vpc_id                  = "${aws_vpc.vpc01.id}"
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags {
     Name = "subnet01"
   }
 }
-output "subnet_id" { value = "${aws_subnet.subnet01.id}" }
+output "subnet01_id" { value = "${aws_subnet.subnet01.id}" }
+
+resource "aws_subnet" "subnet02" {
+  vpc_id                  = "${aws_vpc.vpc01.id}"
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+  tags {
+    Name = "subnet02"
+  }
+}
+output "subnet02_id" { value = "${aws_subnet.subnet02.id}" }
 
 #### Internet gateways
 ## IG prod
