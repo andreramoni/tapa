cb7e252f5d59e8fd700e366598ed4f884e80b9a0
Andre Ramoni <nospam@Ramoni.local>
2019-03-01 00:30:04 -0300

    .

---
+++ 202-terraform+aws+packer/terraform/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "i01" {
+  ami                    = "${data.aws_ami.MyAMI.id}" 
+  count                  = "1"  
+  key_name               = "${aws_key_pair.kp01.id}"
+  instance_type          = "${var.instance_type}"
+  vpc_security_group_ids = [ "${aws_security_group.sg01.id}" ]
+  subnet_id              = "${aws_subnet.subnet01.id}"
+  tags = {
+    Name = "i01"
+  }
+}
+output "private_ip" { value = "${aws_instance.i01.private_ip}" }
+output "public_ip"  { value = "${aws_instance.i01.public_ip}" }
+
+
---
+++ 202-terraform+aws+packer/terraform/securitygroups.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "sg01" {
+  name = "sg01"
+  vpc_id = "${aws_vpc.vpc01.id}"
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
