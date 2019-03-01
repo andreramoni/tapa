d4f72f275ec06a8271ec158cb454dcd7fd06a162
Andre Ramoni <nospam@Ramoni.local>
2019-02-28 23:00:24 -0300

    .

--- 201-terraform+aws+packer/packer/provision.sh
+++ 201-terraform+aws+packer/packer/provision.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 ##############################
-# This script is already run with sudo.
+# This script is called by packer and
+# already run with sudo.
 
-echo "Provisioning script."
+echo
+echo "-------------------------------------------"
+echo "    Provisioning script."
+echo "    You can use this to execute commands "
+echo "inside the instance to prepare it."
+echo "    You can install packages, bring services"
+echo "up, manage some configuration files etc."
+echo "    In this case, we'll do nothing."
+echo "-------------------------------------------"
+
 
 #yum upgrade -y
+#yum install nginx
 
 
