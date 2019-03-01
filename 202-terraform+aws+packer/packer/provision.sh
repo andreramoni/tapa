034e815eb662f8c267eda7ef65f38cfe580381ea
Andre Ramoni <nospam@Ramoni.local>
2019-03-01 00:20:17 -0300

    .

---
+++ 202-terraform+aws+packer/packer/provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##############################
+# This script is called by packer and
+# already run with sudo.
+
+echo
+echo "-------------------------------------------"
+echo "    Provisioning script."
+echo "    Installing NGInx... "
+echo "-------------------------------------------"
+
+yum install -y epel-release
+yum install -y nginx
+
+
