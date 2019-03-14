a8e12bf45488b2d0fa2c0b1efa6e03cdfd01e31c
Andre Ramoni <nospam@Ramoni.local>
2019-03-14 15:37:03 -0300

    .

--- 301-terraform+aws+packer+ansible+autoscaling/terraform/launchgroup.tf
+++ 301-terraform+aws+packer+ansible+autoscaling/terraform/launchgroup.tf
@@ -36,7 +36,7 @@ resource "aws_autoscaling_group" "app01_asg" {
   min_elb_capacity = "2"
   max_size         = "10"
   min_size         = "2"
-  desired_capacity = "2"
+  #desired_capacity = "2"
 
   health_check_grace_period = 300
   health_check_type         = "ELB"
