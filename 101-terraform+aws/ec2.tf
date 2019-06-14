80569364a7a34aa3a1c500f6456e096361783766
Andre Guimaraes <andre.ramoni@gmail.com>
2019-06-14 15:37:59 -0300

    .

--- 101-terraform+aws/ec2.tf
+++ 101-terraform+aws/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "i01" {
   ami                    = "ami-4bf3d731" 
   count                  = "1"  
   key_name               = "${aws_key_pair.kp01.id}"
+  #disable_api_termination = "false"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [ "${aws_security_group.sg01.id}" ]
   subnet_id              = "${aws_subnet.subnet01.id}"
@@ -11,7 +12,7 @@ resource "aws_instance" "i01" {
     Name = "i01"
   }
 }
-output "private_ip" { value = "${aws_instance.i01.private_ip}" }
-output "public_ip" { value = "${aws_instance.i01.public_ip}" }
 
+output "private_ip" { value = "${aws_instance.i01.private_ip}" }
+output "public_ip"  { value = "${aws_instance.i01.public_ip}"  }
 
